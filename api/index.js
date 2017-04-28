c9e6fd4d12f7f40a847f5934174d87fe230471a1
Martial <martial.jefferson@gmail.com>
2017-04-27 20:29:22 -0400

    more search styles; test unbundled version on mobile

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const soap = require( 'soap-as-promised' );
+const rxAPI = `https://mws.envisionrx.com/rxfunctions.asmx?WSDL`;
+const BaseURL = 'http://mws.envisionrx.com/';
+const username = 'medtrak1';
+const password = 'Med%123';
+
+soap.createClient( rxAPI, { empty: true } )
+  .then( ( client ) => {
+
+    let credentials = {
+      UserCredentials: {
+        UserID: username,
+        Password: password
+      }
+    };
+
+    client.addSoapHeader( credentials, 'UserCredentials', "", "http://mws.envisionrx.com/" );
+
+    return client.GetDrugByName( { "DrugName": "Adderall" } );
+  } )
+  .then( ( result ) => {
+    console.log( `The result was: ${result}` );
+  } )
+  .catch( ( error ) => {
+    console.error( `There was an error! ${error}` );
+  } );
