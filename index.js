b8b51535313369893d1817622121fc71a4f58b94
Martial <martial.jefferson@gmail.com>
2017-05-12 18:02:01 -0400

    perhaps fix click behavior on mobile

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let port = 5010;
 app.use( '/api/v1', api );
 app.use( "/src", static( "./src" ) );
 app.use( "/bower_components", static( "./bower_components" ) );
-app.use( "/", ( req, res, next ) => res.sendFile( "index.html", { root: process.cwd( ) } ) );
+app.use( "/", static( "./build/default" ) );
 const server = app.listen( port, host );
 
 server.on( "listening", z => console.log( `Server listening on http://${server.address().address}:${server.address().port}` ) ).on( "error", err => console.error( err ) );
