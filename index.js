1db61241b8b54967f0df3c4acfecbefdc01ca876
Matthew Kandler <matthew.kandler@gmail.com>
2017-06-26 11:43:17 -0400

    faq page, header styles, ctas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const ex = require( "express" );
 const pkg = require( "./package.json" );
+import path from 'path'
 const { name } = pkg;
 const { Router } = ex;
 const app = ex( );
@@ -10,6 +11,7 @@ let port = 5010;
 app.use( '/api/v1', api );
 app.use( "/src", ex.static( "./src" ) );
 app.use( "/bower_components", ex.static( "./bower_components" ) );
+app.use(ex.static(path.join(__dirname, 'public')))
 app.use( "/", ex.static( "." ) );
 const server = app.listen(process.env.PORT || 5000);
 
